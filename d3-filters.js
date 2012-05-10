25dbcda45e7900fb2c327faa47106a06d78bf9e1
Nils <nils.schlomann@gmail.com>
2012-05-10 11:21:00 -0700

    added first bunch of filters to the file

---
+++ d3-filters.js
@@ -0,0 +1,43 @@
+        var defs   = svg.append('defs')          
+        var filter = defs.append('filter')
+            .attr('id','myFilterName')
+
+        var filter_elem1 = filter.append('feGaussianBlur')
+            .attr('result','result8')
+            .attr('id','fe1')
+            .attr('stdDeviation','3.31')
+
+
+        var filter_elem2 = filter.append('feTurbulence')
+            .attr('result','result7')
+            .attr('id','fe2')
+            .attr('seed','27')
+            .attr('type','fractalNoise')
+            .attr('numOctaves','5')
+            .attr('baseFrequency','0.031')
+
+        var filter_elem3 = filter.append('feComposite')
+            .attr('result','result6')
+            .attr('in','result7')
+            .attr('in2','result8')
+            .attr('id','fe3')
+            .attr('operator','over')
+
+        var filter_elem4 = filter.append('feColorMatrix')
+            .attr('result','result9')
+            .attr('values','1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 6 -4')
+            .attr('id','fe4')
+
+        var filter_elem5 = filter.append('feDisplacementMap')
+            .attr('result','result4')
+            .attr('in','result7')
+            .attr('in2','result9')
+            .attr('id','fe5')
+            .attr('xChannelSelector','A')
+            .attr('yChannelSelector','A')
+            .attr('scale',"26.75")
+
+        var filter_elem9 =filter.append('feBlend')
+            .attr('mode','multiply')
+            .attr('in','fbSourceGraphic')
+            .attr('in2','result91')
